1ad79063dec1cb72ab8bbaef8aab8743aab9921c
Iylia <IyliaH@rabobank.corp>
2020-07-27 15:33:51 +0200

    Last update

--- src/com/RaboAndCap/tutorials/addToCart.java
+++ src/com/RaboAndCap/tutorials/addToCart.java
@@ -19,8 +19,11 @@ public class addToCart extends availableItems{
     }
 
     public void currentItemsInCart() {
-        float cartPrice;
-
+        System.out.println("The total value of your cart is " + cartPrice + "\n");
+        System.out.println("While holding these items in your cart " + "\n" + al);
+        System.out.println("Would you like to pay or adjust your cart? \n");
+        System.out.println("To pay, type 'Pay'. To adjust your cart type 'Adjust'");
+        payOrEdit();
     }
 
 
@@ -32,6 +35,7 @@ public class addToCart extends availableItems{
         System.out.println("Which of these items would you like to add to your cart? ");
         System.out.println("You can choose from the following products \n");
         System.out.println("Type exit to close the interface");
+        System.out.println("Type done to proceed to checkout");
         itemLoop();
 
         chooseItems();
@@ -39,49 +43,59 @@ public class addToCart extends availableItems{
 
     public void chooseItems() {
         boolean quit = false;
-        if (al.size() > 3) {
-            quit = true;
-            System.out.println("We can't handle more than 3 items, please proceed to checkout");
+            do {
+                String x = scan.next();
+                switch (x) {
+                    case "Cereals":
+                        System.out.println("You've added Cereals to your cart");
+                        al.add(items.products[0]);
+                        System.out.println();
+                        System.out.println(al);
+                        cartPrice = cartPrice + items.productPrices[0];
+                        System.out.println(cartPrice);
+                        break;
+                    case "Soap":
+                        System.out.println("You've added Soap to your cart");
+                        al.add(items.products[1]);
+                        alPrice.add(items.productPrices[1]);
+                        System.out.println(al);
+                        cartPrice = cartPrice + items.productPrices[1];
+                        System.out.println(cartPrice);
+                        break;
+                    case "Vegetables":
+                        System.out.println("You've added Chinese Vegetables to your cart");
+                        al.add(items.products[2]);
+                        System.out.println(al);
+                        cartPrice = cartPrice + items.productPrices[2];
+                        System.out.println(cartPrice);
+                        break;
+                    case "Diapers":
+                        System.out.println("You've added Diapers to your cart");
+                        al.add(items.products[3]);
+                        cartPrice = cartPrice + items.productPrices[3];
+                        System.out.println(cartPrice);
+                        break;
+                    case "Yoghurt":
+                        System.out.println("You've added Yoghurt to your cart");
+                        al.add(items.products[4]);
+                        cartPrice = cartPrice + items.productPrices[4];
+                        System.out.println(cartPrice);
+                        break;
+                    default:
+                        System.out.println("Please try again");
+                        break;
+                    case "Exit":
+                        quit = true;
+                    case "Done":
+                        quit = true;
+                        currentItemsInCart();
+                }
+            } while (!quit);
 
-        }
-        do {
-        String x = scan.next();
-        for (float productsInStoragePrice : items.productPrices)
-        switch (x) {
-            case "Cereals":
-                System.out.println("You've added Cereals to your cart");
-                al.add(items.products[0]);
-                System.out.println();
-                System.out.println(al);
-                break;
-            case "Soap":
-                System.out.println("You've added Soap to your cart");
-                al.add(items.products[1]);
-                alPrice.add(items.productPrices[1]);
-                System.out.println(al);
-                break;
-            case "Vegetables":
-                System.out.println("You've added Chinese Vegetables to your cart");
-                al.add(items.products[2]);
-                System.out.println(al);
-                break;
-            case "Diapers":
-                System.out.println("You've added Diapers to your cart");
-                al.add(items.products[3]);
-                alPrice.add(items.productPrices[3]);
-                break;
-            case "Yoghurt":
-                System.out.println("You've added Yoghurt to your cart");
-                al.add(items.products[4]);
-                alPrice.add(items.productPrices[4]);
-                break;
-            default:
-                System.out.println("Please try again");
-                break;
-            case "Exit":
-                quit = true;
-        }
-        } while (!quit);
+
+    }
+
+    public void payOrEdit() {
 
     }
 
