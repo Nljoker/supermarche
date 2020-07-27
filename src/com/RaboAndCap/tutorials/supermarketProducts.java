fddaf2177a8f3f16b61c7538f8f166b062daf8be
Iylia <IyliaH@rabobank.corp>
2020-07-27 13:22:13 +0200

    second commit

--- src/com/RaboAndCap/tutorials/supermarketProducts.java
+++ src/com/RaboAndCap/tutorials/supermarketProducts.java
@@ -1,8 +1,17 @@
 package com.RaboAndCap.tutorials;
 
-public class supermarketProducts {
-    private float priceInEuros;
-    private String productName;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+public class supermarketProducts implements supermarketProductsInterface{
 
+    @Override
+    public String productName(String name) {
+        return name;
+    }
+
+    @Override
+    public float productPrice(float price) {
+        return price;
+    }
 }
---
+++ src/com/RaboAndCap/tutorials/supermarketProductsInterface.java
@@ -0,0 +1,7 @@
+package com.RaboAndCap.tutorials;
+
+public interface supermarketProductsInterface {
+    public String productName(String x);
+    public float productPrice(float x);
+
+}
